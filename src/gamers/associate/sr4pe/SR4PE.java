ae8ef0f1f72b337b9e04e861a3736b6a1a70f5a1
vcarluer <vcarluer@gmail.com>
2013-10-28 18:34:52 +0100

    parse upper case and extract to files

--- src/gamers/associate/sr4pe/SR4PE.java
+++ src/gamers/associate/sr4pe/SR4PE.java
@@ -1,10 +1,13 @@
 package gamers.associate.sr4pe;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
 import java.io.BufferedInputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
@@ -38,7 +41,7 @@ public class SR4PE {
 			File pdfFile = new File(pdfInPath);			
 			File outFile = new File(outPath);
 			File outFileHtml = new File(outPathHtml);
-			InputStream stream = new BufferedInputStream(new FileInputStream(pdfFile));
+			InputStream stream = new FileInputStream(pdfInPath);
 			PDFParser parser = new PDFParser(stream, new RandomAccessBuffer(), true);
 			parser.parse();
 			PDDocument doc = parser.getPDDocument();
@@ -46,7 +49,7 @@ public class SR4PE {
 				doc.openProtection(new StandardDecryptionMaterial("coolcool"));
 			}
 
-			FileWriter writer = new FileWriter(outFile);		
+			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(outPath), "UTF-8");
 
 			PDFTextStripper textStripper = new PDFTextStripper();
 			textStripper.setStartPage(start);
@@ -64,14 +67,62 @@ public class SR4PE {
 			writerHtml.close();*/
 
 			doc.close();
+
+
+			splitText(outPath);
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 			System.out.println(ex.getStackTrace());
 		}
+	}
+	
+	public static void splitText(String inPath) {
+		File inFile = new File(inPath);
+		try
+		{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inPath), "UTF-8"));
+			int upperCount = 0;
+			String line = reader.readLine();
 
+			FileOutputStream fos = null;
+			OutputStreamWriter out = null;
+			while (line != null) { 
+				boolean isUpper = true;
+				for (char c : line.toCharArray()) {
+					if (Character.isLetter(c) && !Character.isUpperCase(c)) {
+						isUpper = false;
+						break;
+					}
+				}	
+		
+				if (isUpper) {
+					// this is a new paragraph
+					upperCount++;
+					System.out.println(line);
+					if (out != null) {
+						out.close();
 
+					}
+
+					fos = new FileOutputStream("temp\\" + line.replaceAll("[\\s+, *]", "") + ".txt");
+					out = new OutputStreamWriter(fos, "UTF-8");
+				}
+
+				out.write(line + "\n");
+
+				line = reader.readLine();
+			}
+
+			if (out != null) {
+				out.close();
+			}
+
+			System.out.println(String.valueOf(upperCount));
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
 
